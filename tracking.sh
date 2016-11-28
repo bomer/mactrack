e18bcf4b79f16f6d7f29810e60f5ba76dc548acc
James O'Toole <jimmeyotoole@gmail.com>
2016-11-28 18:09:51 +1100

    setup symlink to notify from desktop in mac

--- tracking.sh
+++ tracking.sh
@@ -1,9 +1,17 @@
 url=`cat url` 
+email="jimmeyotoole@gmail.com"
+
 
 if [ ! -f "url" ]
 then
     echo "A file called url does not exist. Create it and paste ur URL into it to being tracking"
     exit
+fi
+
+#adds a quick task to 'notify' the user
+if [ ! -f "/usr/local/bin/notify"]
+	echo '/usr/bin/osascript -e "display notification \"$*\""' > /usr/local/bin/notify
+	chmod +x /usr/local/bin/notify
 fi
 
 echo Beginning Tracking of changes to $url
@@ -25,7 +33,7 @@ while true; do
 	else
 		echo File has changed
 		say oy mate zoh mah god seriously tracking has updated		
-		echo "It has updated as of `Date` \n  " | mail -s "Dude check your tracking" jimmeyotoole@gmail.com
+		echo "It has updated as of `Date` \n  " | mail -s "Dude check your tracking" $email
 		notify tracking updated
 	fi
 
